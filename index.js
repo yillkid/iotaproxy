ddd32cfdf5913597a697541c5ec765d33e6146e4
yillkid <yillkid@gmail.com>
2017-10-11 06:02:20 +0000

    Add local dependecy package

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 /**
  * Simple proxy server for IOTA.
  * Relays commands to the IOTA Tangle, but intercepts attachToTangle commands and performs PoW locally.
- * 
- * This proxy server useful for when you want to perform transfers with iota.lib.js in Node but do not 
+ *
+ * This proxy server useful for when you want to perform transfers with iota.lib.js in Node but do not
  * have access to a full node that grants you access to the necessary attachToTangle commands.
  */
 
@@ -11,10 +11,10 @@ var iotaProxy = require('./lib/iotaproxy.js');
 
 iotaProxy.start(
   {
-    host: 'http://node0.puyuma.org', 
-    port: 14265, 
+    host: 'http://node0.puyuma.org',
+    port: 14265,
     localPort: 14266,
-    overrideAttachToTangle: true
+    overrideAttachToTangle: true,
+    timeout: 15
   }
 );
-
